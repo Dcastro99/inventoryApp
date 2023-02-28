a41b9bf803e24fcb4de5f36ae411e6d2925bf95f
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-02-27 18:13:18 -0800

    added backend basics

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+'use strict';
+//;/  (O_O) /;//
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (request, response) => {
+  response.send('TESTING INVENTORY_APP!');
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
