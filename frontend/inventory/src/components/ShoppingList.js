961d6448d1251d1918d3f50a470570b3c3f64e6d
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-03-26 22:13:34 -0700

    added completed list

--- frontend/inventory/src/components/ShoppingList.js
+++ frontend/inventory/src/components/ShoppingList.js
@@ -22,10 +22,11 @@ export default function ShoppingList({ cartItems, clearCart, updateCart, decreme
   const [open, setOpen] = useState(false);
   const [newQty, setNewQty] = useState(0);
   const { updateProduct, products } = useContext(ProductContext);
-  let itemsAlert;
+  // let completedItem = [];
+  const [completedItem, setCompletedItem] = useState([]);
   // let decrementSum;
   // let deleteAll;
-
+  console.log('completedItem', completedItem)
   console.log('newItem in Cart', newItem)
   console.log('products in inventory', products)
   const handleClickOpen = () => {
@@ -72,6 +73,21 @@ export default function ShoppingList({ cartItems, clearCart, updateCart, decreme
             x.qty = sum,
             x.id,
           );
+          let itemToComplete = completedItem.find((item) => item.productName === x.productName);
+          if (itemToComplete) {
+            itemToComplete.productName = x.productName;
+            itemToComplete.uom = x.uom;
+            itemToComplete.qty = x.qty;
+            itemToComplete.id = x.id;
+            itemToComplete.newId = x.newId;
+            itemToComplete.checked = x.checked;
+
+            setCompletedItem([...completedItem]);
+          }
+          else {
+            setCompletedItem([...completedItem, x])
+          }
+
         } else if (
           !x.checked
         ) {
@@ -110,9 +126,20 @@ export default function ShoppingList({ cartItems, clearCart, updateCart, decreme
   }
 
 
+
   const handleClearAll = () => {
-    // deleteAll = newItem.map((x) => {
-    //   let item = products.map((product) => {
+    newItem.map((x) => {
+      if (x.checked === true) {
+
+        setNewItem([])
+        setCompletedItem([])
+      }
+      else {
+        setCompletedItem([])
+      }
+      return x;
+    })
+    // ) : (setCompletedItem([]))
     //     if (x.id === product.id) {
 
 
@@ -141,7 +168,6 @@ export default function ShoppingList({ cartItems, clearCart, updateCart, decreme
     // });
 
 
-    clearCart();
   }
 
 
@@ -195,60 +221,93 @@ export default function ShoppingList({ cartItems, clearCart, updateCart, decreme
           {newItem.map((item) => {
             return (
               <>
-                < Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 1 }}>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', maxWidth: 500, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', padding: 2, borderRadius: 2 }}>
-                    {/* {console.log('item.checked', item.checked)} */}
-                    {item.checked === true ? (<Typography sx={{ fontWeight: 'bold', fontSize: 20, }}><del>{item.productName}</del></Typography>) : (<Typography sx={{ fontWeight: 'bold', fontSize: 20, }}>{item.productName}</Typography>)}
+                {item.checked === false ? (
+                  < Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 1 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', maxWidth: 500, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', padding: 2, borderRadius: 2 }}>
+                      {/* {console.log('item.checked', item.checked)} */}
+                      {item.checked === true ? (<Typography sx={{ fontWeight: 'bold', fontSize: 20, }}><del>{item.productName}</del></Typography>) : (<Typography sx={{ fontWeight: 'bold', fontSize: 20, }}>{item.productName}</Typography>)}
 
-                    {/* {console.log('itemzzzz', item)} */}
-                    {/* <Typography sx={{ color: 'lightGray' }}>{item.qty}</Typography> */}
-                    {item.checked === true ?
-                      (<TextField label="unselect" type='number' name='product_quantity' disabled error required onChange={(e) => handleQty(e)} sx={{
-                        width: '80px',
-                      }} />) : (<TextField label="Qty" type='number' name='product_quantity' required onChange={(e) => handleQty(e)} sx={{
-                        width: '80px',
-                      }} />)}
-                    {item.checked === false ? (<CheckBoxOutlineBlankOutlinedIcon onClick={() => handleCheck(item)} />) : (<CheckBoxOutlinedIcon onClick={() => handleCheck(item)} />)}
-                    <div>
-                      <Button sx={{
-                        backgroundColor: 'white',
-                        color: '#FF7F50',
-                        borderRadius: '10px',
-                        divShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
-                        '&:hover': {
-                          backgroundColor: 'smokeWhite',
-                          color: 'red',
-                        },
-                        margin: 1
-                      }} onClick={() => deleteHandler(item)}>Delete</Button>
-                    </div>
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', maxWidth: 175, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', padding: 4, borderRadius: 2, marginLeft: 5 }}>
-                    Current Qty:
-                    <Typography sx={{ fontWeight: 'bold', }}>{item.qty}</Typography>
-                  </Box>
-                </Box>
+                      {/* {console.log('itemzzzz', item)} */}
+                      {/* <Typography sx={{ color: 'lightGray' }}>{item.qty}</Typography> */}
+                      {item.checked === true ?
+                        (<TextField label="unselect" type='number' name='product_quantity' disabled error required onChange={(e) => handleQty(e)} sx={{
+                          width: '80px',
+                        }} />) : (<TextField label="Qty" type='number' name='product_quantity' required onChange={(e) => handleQty(e)} sx={{
+                          width: '80px',
+                        }} />)}
+                      {item.checked === false ? (<CheckBoxOutlineBlankOutlinedIcon onClick={() => handleCheck(item)} />) : (<CheckBoxOutlinedIcon onClick={() => handleCheck(item)} />)}
+                      <div>
+                        <Button sx={{
+                          backgroundColor: 'white',
+                          color: '#FF7F50',
+                          borderRadius: '10px',
+                          divShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+                          '&:hover': {
+                            backgroundColor: 'smokeWhite',
+                            color: 'red',
+                          },
+                          margin: 1
+                        }} onClick={() => deleteHandler(item)}>Delete</Button>
+                      </div>
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', maxWidth: 175, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', padding: 4, borderRadius: 2, marginLeft: 5 }}>
+                      Current Qty:
+                      <Typography sx={{ fontWeight: 'bold', }}>{item.qty}</Typography>
+                    </Box>
+                  </Box>) : (<> </>)
+                }
 
               </>
             )
           })
           }
         </>) : (<Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 20 }}><Typography sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '140px', maxWidth: 360, fontSize: 20, fontWeight: 'bold', padding: 5, borderRadius: 5, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', backgroundColor: 'whitesmoke' }}>Your shopping list is empty</Typography></Box>)}
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Button sx={{
-            backgroundColor: 'white',
-            color: '#626D75',
-            borderRadius: '10px',
-            divShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
-            '&:hover': {
-              backgroundColor: 'white',
-              color: 'black',
-            }, marginTop: 10,
-            maxWidth: 360,
-          }} onClick={handleClearAll}>Clear All</Button>
-        </Box>
+        {completedItem.length > 0 ? (<>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: 10, marginBottom: 10, padding: 5, borderRadius: 5, width: 800, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)' }}>
+              <Typography sx={{ fontWeight: 'bold', fontSize: 20, marginBottom: 5 }}>Completed Items</Typography>
+
+
+              {completedItem.map((x) => {
+                console.log('x.productName', x.productName)
+                return (<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 1, width: '50%' }}>
+                  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', maxWidth: 500, boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', padding: 2, borderRadius: 2 }}>
+                    <Typography sx={{ fontWeight: 'bold', fontSize: 20, }}>{x.productName}</Typography>
+                    <Button sx={{
+                      backgroundColor: 'WhiteSmoke',
+                      color: '#626D75',
+                      borderRadius: '10px',
+                      divShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+                      '&:hover': {
+                        backgroundColor: 'lightGray',
+                        color: '#FF7F50',
+                      },
+                    }}><Typography sx={{ fontWeight: 'bold', fontSize: 20, }}>Undo</Typography></Button>
+                  </Box>
+                </Box>)
+              })}
+
+
+
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Button sx={{
+                  backgroundColor: 'white',
+                  color: '#FF7F50',
+                  borderRadius: '10px',
+                  divShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+                  '&:hover': {
+                    backgroundColor: 'white',
+                    color: 'black',
+                  }, marginTop: 10,
+                  maxWidth: 360,
+                }} onClick={handleClearAll}>Clear All</Button>
+              </Box>
+            </Box>
+
+          </Box>
+        </>) : (<></>)}
       </Dialog>
-      {itemsAlert}
     </div >
   );
 }
