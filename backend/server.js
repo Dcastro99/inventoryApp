f966906c27427d7fbdbf2c1a77d0f888fcea41e2
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-04-10 17:21:04 -0700

    added server files

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,32 @@
 'use strict';
-//;/  (O_O) /;//
+
+//----------------DOTENV Config----------------//
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
+//----------------CRUD----------------//
+
+
+// -----------APP USING EXPRESS & JSON -------------//
+const PORT = process.env.PORT || 3002;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3002;
+
+//------------- ERROR HANDLING -------------//
+
+const notFoundHandler = require('./src/handlers/404.js');
+const errorHandler = require('./src/handlers/500.js');
+
 
 app.get('/', (request, response) => {
   response.send('TESTING INVENTORY_APP!');
 });
+
+app.get('*', notFoundHandler);
+app.use(errorHandler);
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
